7378f63ae542e780eaa8a9f70df8b80c582ee380
Gustavo <gustavobispocosta5521@gmail.com>
2026-03-12 20:34:07 -0300

    Conceitos: Modules, Controllers e Services

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('home') // /home
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  //Método da solicitação -> Ler (Read) -> CRUD
+  // /home/hello
+  @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    const retorno = 'Retorno'
+    return retorno;
+  }
+
+  @Get('exemplo')
+  exemplo() {
+    return this.appService.solucionaExemplo();
   }
 }
--- src/conceitos-manual/conceitos-manual.module.ts
+++ src/conceitos-manual/conceitos-manual.module.ts
@@ -1,4 +1,9 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { ConceitosManualController } from './conceitos-manual.controller';
+import { ConceitosManualService } from './conceitos-manual.service';
 
-@Module({})
-export class ConceitosManualModule{}
+@Module({
+  controllers: [ConceitosManualController],
+  providers: [ConceitosManualService],
+})
+export class ConceitosManualModule {}

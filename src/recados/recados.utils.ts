27d5143f7d3425aa1986870d9f70a1f57087a956
Gustavo <gustavobispocosta5521@gmail.com>
2026-05-08 11:33:29 -0300

    Usando Mock para criar um valor de testes

--- src/recados/recados.utils.ts
+++ src/recados/recados.utils.ts
@@ -3,7 +3,15 @@ import { Injectable }  from '@nestjs/common'
 @Injectable()
 export class RecadosUtils {
     inverteString(str: string){
-        //Luiz -> ziuL
+        console.log("Não é Mock")
         return str.split('').reverse().join()
+    }
+}
+
+@Injectable()
+export class RecadosUtilsMock {
+    inverteString(){
+        console.log("Passei no Mock")
+        return 'Bla Bla Bla';
     }
 }

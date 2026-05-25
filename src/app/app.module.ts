415b4b0f7b737d0c0f1ad30bce60a5505c6a222a
Gustavo <gustavobispocosta5521@gmail.com>
2026-05-25 15:24:30 -0300

    Puxando as configurações do banco de dados pelo .env com nestjs/config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { AppService } from './app.service';
 import { RecadosModule } from '../recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from '../pessoas/pessoas.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'postgres',
-      password: '123456',
-      autoLoadEntities: true, //Carrega entidades sem precisar especifica-las
-      synchronize: true //Sincroniza tudo com o DB, isso não deve ser usado em Produção
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined,
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), //Carrega entidades sem precisar especifica-las
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE) //Sincroniza tudo com o DB, isso não deve ser usado em Produção
     }),
     RecadosModule,
     PessoasModule,
@@ -26,4 +28,8 @@ import { PessoasModule } from '../pessoas/pessoas.module';
   exports: [],
 })
 export class AppModule {
+  constructor(){
+    console.log(process.env.ESSA_E_UMA_VARIAVEL_1)
+    console.log(process.env.ESSA_E_UMA_VARIAVEL_2)
+  }
 }

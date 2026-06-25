68fb7333334fb0edfa6213a8209e9e5fa71e7ad9
Gustavo <gustavobispocosta5521@gmail.com>
2026-06-25 11:27:13 -0300

    Utilizando o Multer do Express para upload de arquivos no client.rest

--- src/pessoas/pessoas.controller.ts
+++ src/pessoas/pessoas.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth.token.guard';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
 import { TokenPayloadParam } from 'src/auth/params/token.payload.param';
+import { FileInterceptor } from '@nestjs/platform-express';
+import type { Express } from 'express';
+import {Multer} from 'multer'
+
 
 @Controller('pessoas')
 export class PessoasController {
@@ -41,4 +45,17 @@ export class PessoasController {
   remove(@Param('id') id: string, @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
     return this.pessoasService.remove(+id, tokenPayload);
   }
+
+  @UseGuards(AuthTokenGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload-picture')
+  uploadPicture(@UploadedFile() file:Express.Multer.File, @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
+    return {
+      "fieldname": file.fieldname,
+    "originalname": file.originalname,
+    "mimetype": file.mimetype,
+    "buffer": {},
+    "size": file.size,
+  }
+}
 }
